9a9b6874524903a358dbb3e8ad37ba57b735d242
Janhavi Anantprakash Kharmale <janhavianantprakash@usf.edu>
2024-03-06 16:14:55 -0500

    Created Car objects and Program

--- Car.cs
+++ Car.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Vehicle
 {
-    internal class Car
+    internal class Car : Vehicle, RoadRules
     {
+        public string horn()
+        {
+            return "Horn ok Please";
+        }
+
+        public string speedDown()
+        {
+            return "Speed limit reached, Speed Down!";
+        }
+
+        public string speedUp()
+        {
+            return "Speed Up Guys";
+        }
+
+        public string CarType;
+        public int SeatNo;
+
+        public Car() 
+        {
+            CarType = "Sedan";
+            SeatNo = 4;
+        }
+
+        public Car(string CarType , int SeatNo )
+        { 
+            this.CarType = CarType;
+            this.SeatNo = SeatNo;
+        }
+
+        public Car( string CarType, int SeatNo , int Make , string Model, int Price)
+            :base(Make,Model,Price)
+        {
+            this.CarType = CarType;
+            this.SeatNo = SeatNo;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@
             Console.WriteLine("Hello, World!");
             Vehicle v1= new Vehicle();
             Console.WriteLine(v1.getModel());
+
+            Car car1 = new Car();
+            Car car2 = new Car("SUV", 5);
+            Car car3 = new Car("Hatchback", 4, 2022, "XYZ", 25000);
+
+            Console.WriteLine("Car 1: Car Type - {0}, Seat No - {1}", car1.CarType, car1.SeatNo);
+            Console.WriteLine("Car 2: Car Type - {0}, Seat No - {1}", car2.CarType, car2.SeatNo);
+            Console.WriteLine("Car 3: Car Type - {0}, Seat No - {1}", car3.CarType, car3.SeatNo);
+            Console.WriteLine("Car 3: Make - {0}, Model - {1}, Price - {2}", car3.make, car3.model, car3.price);
+
+            Console.WriteLine(car1.horn());
+            Console.WriteLine(car2.speedDown());
+            Console.WriteLine(car3.speedUp());
         }
     }
 }
